4bc44b827eb9e075e0e868a5154a1cf5e5328c43
Devin Lange <devinscottlange@gmail.com>
2020-01-24 17:13:38 -0700

    add (untested) script for transforming csv file to json file

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # image-embedding-data
-embedding data for vizualizations
+
+This file describes the format required to run csvToJson.py in order to
+update the JSON files appropriately.
+
+
+
+dataset_projection.csv
+
+
+dataset -
+    The display name of the dataset. This appears in
+    the top left corner of the visualization. For
+    example. "Particles - All"
+
+projection -
+    The display name of the projection. This appears
+    in the top right corner of the visualization. For
+    example. "L1 [64]"
+
+In this example the full filename would be "Particles - All_L1 [64].csv"
+
+The content of the file should be a csv file containing the following information.
+
+The first row contains text labels for the columns. The columns should contain
+the following.
+
+column 1  - the X positions for all the points.
+column 2  - the Y position for all the points.
+column 3+ - any attribute that you want to visualize. This can be seen in the
+            visualization in the top left, below the datasets, after the 
+            attribute label.
+
+So for the "Particles - All_L1 [64].csv" example, the first 5 lines of the file
+could look like the following.
+
+x,y,max_field,avg_field,scattering,extinction,absorption
+3.87372,1.40561,3.34469,2.05705,-33.3863,-31.7639,-31.9838
+-2.92122,-0.209223,1.02112,0.399764,-40.7863,-38.0932,-38.1633
+-1.96136,-0.0598162,1.31549,0.569668,-38.251,-37.445,-38.0368
+2.6516,-0.143876,2.65261,1.28398,-33.8529,-31.7579,-31.8892
---
+++ csvToJson.py
@@ -0,0 +1,127 @@
+from typing import List, IO, TextIO, Dict
+import json
+import os
+import shutil
+
+IN_FOLDER: str = './in/'
+TOP_LEVEL_STRUCTURE_FILENAME: str = 'examples.json'
+PROCESSED_FOLDER: str = './processed/'
+
+
+def main():
+    filename = 'justPCA.csv'
+    topLevelStructure = json.load(open(TOP_LEVEL_STRUCTURE_FILENAME))
+
+    for filename in os.listdir(IN_FOLDER):
+        baseName, extension = os.path.splitext(filename)
+        if extension in {'.txt', '.csv'}:
+            addFile(IN_FOLDER + filename, topLevelStructure)
+            shutil.move(IN_FOLDER + filename, PROCESSED_FOLDER + filename)
+
+    saveTopLevelStructure(topLevelStructure)
+    return
+
+def saveTopLevelStructure(topLevelStructure: List) -> None:
+    outfile = open(TOP_LEVEL_STRUCTURE_FILENAME, 'w')
+    outfile.write(json.dumps(topLevelStructure, indent=4))
+    return
+
+def addFile(filename: str, topLevelStructure: List) -> None:
+    baseName: str = filename.split('.')[0]
+    datasetName,  projectionDisplayName = baseName.split('_')
+    datasetObj = findDataset(datasetName, topLevelStructure)
+    folderName = datasetObj['folderName']
+    projectionFilename = findFilename(projectionDisplayName, datasetObj)
+
+    csvFile = open(IN_FOLDER + filename, 'rt')
+    count = 0
+    for line in csvFile.readline():
+         count += 1
+    csvFile.close()
+    csvFile = open(IN_FOLDER + filename, 'rt')
+    csvToJson(csvFile, count, folderName + "/" + projectionFilename)
+    return
+
+def findDataset(displayName: str, topLevelStructure: List) -> Dict:
+    for dataset in topLevelStructure:
+        if dataset['displayName'] == displayName:
+            return dataset
+    newDataset = {}
+    newDataset['displayName'] = displayName
+    folderName = displayName
+    index = 2
+    while os.path.exists(displayName):
+        folderName = displayName + " (" + str(index) + ")"
+        index += 1
+    newDataset['folderName'] = folderName
+    newDataset['imageWidth'] = 80
+    newDataset['imageHeight'] = 80
+    newDataset['projectionList'] = []
+    printNewFolderMessage(folderName)
+    return newDataset
+
+def findFilename(displayName: str, datasetObj: Dict) -> str:
+    folderName = datasetObj['folderName']
+    for projection in datasetObj['projectionList']:
+        if projection['displayName'] == displayName:
+            return projection['filename']
+    newProj = {}
+    newProj['displayName'] = displayName
+    filename = displayName
+    index = 2
+    while os.path.exists(folderName + "/" + filename + ".json"):
+        filename = displayName + " (" + str(index) + ").json"
+        index += 1
+    filename = filename + ".json"
+    newProj['filename'] = filename
+    datasetObj['projectionList'].append(newProj)
+    return filename
+
+def printNewFolderMessage(newFolderName: str) -> None:
+    print("Warning! - New dataset folder (" + newFolderName + ") added! You must do the following manually.")
+    print("\t1. Set the imageWidth and imageHeight values in " + TOP_LEVEL_STRUCTURE_FILENAME)
+    print("\t2. Add and imageLookup.json file to " + newFolderName +".")
+    print('\t3. Add a new tiledImg.png file to ' + newFolderName + '.')
+    return
+
+def csvToJson(csvFile, numRows: int, outFilename: str) -> None:
+    # x,y,attr1,attr2,...,attrN
+    labels = csvFile.readline().rstrip()
+    labels = labels.split(',')
+
+    maxNameLength = len(str(numRows))
+
+    jsonList: List[dict] = []
+
+    index = 0
+    line = csvFile.readline()
+    while line:
+        line = line.rstrip()
+        line = line.split(',')
+        line = [float(x) for x in line]
+
+        thisDict = {}
+        thisDict['x'] = line[0]
+        thisDict['y'] = line[1]
+        imgName = str(index)
+        while len(imgName) < maxNameLength:
+            imgName = '0' + imgName
+
+        thisDict['image'] = imgName + '.png'
+
+        attributeDict = {}
+        for i in range(2, len(line)):
+            name = labels[i]
+            attributeDict[name] = {'displayName': name, 'value': line[i]}
+        thisDict['attributes'] = attributeDict
+        jsonList.append(thisDict)
+
+        index += 1
+        line = csvFile.readline()
+
+    outFile = open(outFilename, 'w')
+    outFile.write(json.dumps(jsonList))
+    return
+
+if __name__ == "__main__":
+    main()
